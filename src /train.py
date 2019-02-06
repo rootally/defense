2c41070a2e40d50b8e6ba69d091eaca59dd758e7
DwaraknathT <dwarakasharma@gmail.com>
2019-02-06 19:41:29 +0530

    minor changes

--- src /train.py
+++ src /train.py
@@ -13,8 +13,9 @@ import torchvision.transforms as transforms
 import os
 import argparse
 
-from resnet import ResNet18 
+from models.resnet import ResNet18 
 from utils import progress_bar
+from load_data import get_data
 
 
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
@@ -27,29 +28,8 @@ device = 'cuda' if torch.cuda.is_available() else 'cpu'
 best_acc = 0  # best test accuracy
 start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 
-# function to get data 
-def get_data():
-    transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
-
-    transform_test = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-    ])
-
-    trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=2)
-
-    testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=2)
-
-    classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-
-    return trainloader, testloader
+# get the data 
+trainloader, testloader = get_data()
 
 """Fucntion to instantiate the resnet model and return it"""
 def create_model():
@@ -131,7 +111,7 @@ def test(epoch, testloader, net, criterion, optimizer):
         best_acc = acc
 
 
-def train_test():
+if __name__ == "__main__":
     net, criterion, optimizer = create_model()
     trainloader, testloader = get_data()
     for epoch in range(start_epoch, start_epoch+args.num_epochs):
