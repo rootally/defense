c6ac1384bb753e147bc645748de51db1be68dc16
Siddhartha Rao Kamalakara <srk97c@gmail.com>
2019-03-06 11:40:03 +0800

    Refactor and fixes

--- src/utils/dropout.py
+++ src/utils/dropout.py
@@ -6,15 +6,15 @@ Targeted Dropout by Aidan N. Gomez, Ivan Zhang, Kevin Swersky, Yarin Gal, and Ge
 The code release for the same.
 """
 
-def targeted_weight_droput(w, params, is_training):
+def targeted_weight_dropout(w, params, is_training):
     drop_rate = params.drop_rate
     targ_perc = params.targ_rate
     w_shape = list(w.size())
-    w = w.view(-1,w_shape[0])
+    w = w.view(w_shape[0], -1).transpose(0, 1)
     norm = torch.abs(w)
-    idx = int(targ_perc * w_shape[1])
+    idx = int(targ_perc * w.shape[0])
     threshold = (torch.sort(norm, dim=0)[0])[idx]
-    mask = norm < (threshold[0])
+    mask = norm < (threshold[None, :])
 
     if not is_training:
         #Inference
@@ -22,18 +22,18 @@ def targeted_weight_droput(w, params, is_training):
         w = w.view(w_shape)
         return w
 
-    mask = (torch.empty(list(w.size())) < drop_rate) & mask
+    mask = (torch.empty(list(w.size())).uniform_(0,1) < drop_rate) & mask
     mask = mask.float()
     w = (1. - mask) * w
-    w = w.view(w, w_shape)
+    w = w.view(w_shape)
     return w
 
 def targeted_unit_dropout(w, params, is_training):
     drop_rate = params.drop_rate
     targ_perc = params.targ_rate
-    w_shape = list(w.size())
 
-    w = w.view(-1, w_shape[0])
+    w_shape = list(w.size())
+    w = w.view(w_shape[0], -1).transpose(0, 1)
     norm = torch.norm(w, dim=0)
     idx = int(targ_perc * int(w.shape[1]))
     sorted_norms = torch.sort(norm)
@@ -44,5 +44,5 @@ def targeted_unit_dropout(w, params, is_training):
                         torch.ones(w.shape, dtype=torch.float32),
                         torch.zeros(w.shape, dtype = torch.float32))
     x = (1-mask) * w
-    x = x.view(w_shape)
+    x = x.transpose(0, 1).view(w_shape)
     return x
